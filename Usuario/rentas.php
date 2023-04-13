69414e8191a3c659d387abb245df3b040dbcc1ad
-DaniVH- <97567256+DaniVH7@users.noreply.github.com>
2023-04-13 08:42:29 -0600

    Nueva Actualizacion 13/04/2023

--- Usuario/rentas.php
+++ Usuario/rentas.php
@@ -9,6 +9,7 @@
             body{ background-color: #1b262c;}
             table{margin: auto; width: 900px; border-collapse: collapse; }
             table, tr, th, td { border: 1px solid black; justify-content: center; background-color: #0f4c75;}
+            h1{text-align: center; background-color: #0f4c75;color:#BBE1FA;height:70px;}
             td {width: 125px; text-align:center; color:#BBE1Fa; } th{color:black}
             .container{border: 1px solid #BBE1FA;background-color: #1B262C;width: 100%;height: 9vh;}
             .iniciar{width: 100%;height: 100%;}
@@ -72,6 +73,67 @@
                     $conMySQL = null;
                     }
                 ?>
+                <div class="firebase">
+                    <body>
+   <h1>Control de Acceso RFID</h1>
+<table>
+  <thead>
+    <tr>
+      <th>ID de Tarjeta</th>
+      <th>Último Acceso</th>
+      <th>Estado</th> 
+    </tr>
+  </thead>
+  <tbody id="card-table-body">
+    <tr>
+      <td colspan="3" align="center">Cargando...</td>
+    </tr>
+  </tbody>
+</table>
+<script>
+  var cardDataRef = new EventSource("https://quickroom-8f067-default-rtdb.firebaseio.com/rfid-access-control/cards.json");
+
+  cardDataRef.addEventListener('put', function (e) {
+    var json = JSON.parse(e.data);
+    console.log(json);
+
+    if (json.path == "/") {
+      // Clear table body
+      document.getElementById("card-table-body").innerHTML = "";
+
+      for (var key in json.data) {
+        console.log(key);
+        console.log(json.data[key].time);
+        console.log(json.data[key].status);
+
+        var cardRow = document.createElement("tr");
+        var cardIdCell = document.createElement("td");
+        cardIdCell.innerHTML = key;
+        var cardTimeCell = document.createElement("td");
+        var cardTimeString = (json.data[key].time).toLocaleString();
+        cardTimeCell.innerHTML = cardTimeString;
+
+        var cardStatusCell = document.createElement("td");
+        cardStatusCell.innerHTML = json.data[key].status;
+
+        cardRow.appendChild(cardIdCell);
+        cardRow.appendChild(cardTimeCell);
+        cardRow.appendChild(cardStatusCell);
+        document.getElementById("card-table-body").appendChild(cardRow);
+      }
+    } else {
+      var s = json.path.split("/");
+      console.log(s[1]);
+      console.log(json.data.time);
+      console.log(json.data.status);
+
+      var cardTimeCell = document.getElementById(s[1] + "-last-access");
+      var cardTimeString = new Date(json.data.time).toLocaleString();
+      cardTimeCell.innerHTML = cardTimeString;
+    }
+  });
+</script>
+                </div>
  <footer><div class="ayuda"><a href="../../Admin/ayudaadmin.html" style="color: #BBE1FA;">Ayuda
             <div class="antes"><a href="usuario.php" style="color:#BBE1FA; float:right;">Regresar</a></footer>
         </div>
--- Usuario/usuario.php
+++ Usuario/usuario.php
@@ -16,15 +16,15 @@
             <div class="config"></div>
             <div class="busqueda">
                 <div class="texto" >                 
-                    <div class="notify"><a class="notifica" href="notificaciones.php"><img src="../sources/images/campana.png" style="width: 15%; height:10%;" alt="Imagen de notificaciones"></a></div>
-                        <h1 style=margin-left:25%;>Renta de Forma Segura</h1>   
+                    <div class="notify"><a class="notifica" href="notificaciones.php"><img src="../sources/images/campana.png" style="width: 15%; height:8%;" alt="Imagen de notificaciones"></a></div>
+                        <h1 style="margin-left:25%";>Renta de Forma Segura</h1>   
                         
 
                 </div><br>
                 <div class="busquedarapida"><a href="../php/datos.php"><button class="boton"><h1>Datos del Estudiante</h1></button></a></div>
                 <div class="busquedarapida"><a href="nuevabusqueda.html"><button class="boton"><h1>Busqueda</h1></button></a></div>
                 <div class="busquedarapida"><a href="../php/catalogo.php"><button class="boton"><h1>Catalogo</h1></button></a></div>
-                <div class="busquedarapida"><a href="rentas.php"><button class="boton"><h1>Rentas</h1></button></a></div>
+                <div class="busquedarapida"><a href="rentas.php"><button class="boton"><h1>Detalles de Renta y Sensores</h1></button></a></div>
                 <div class="busquedarapida"><a href="pagos.html"><button class="boton"><h1>Pagos</h1></button></a></div>
             </div><br><br><br><br><br><br><br>
         <div class="pie"><a href="ayuda.html" style="color: #BBE1FA;">Ayuda</a>
