0c376dc66208f0c6a1323ae6ea85f580bbe3c336
-DaniVH- <97567256+DaniVH7@users.noreply.github.com>
2023-04-13 08:45:48 -0600

    14/03/2023

--- iot/sensoresAdmin.php
+++ iot/sensoresAdmin.php
@@ -1,68 +1,87 @@
 <!DOCTYPE html>
 <html>
- <meta charset="utf-8">
-    <title>Cuartos Express
-    </title>
-    <link rel="stylesheet" type="text/css" href="../sources/css/administrador/sensores.css">
+  <head>
+    <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../sources/css/administrador/sensores.css">
+    <title>Control de Acceso RFID</title>
     <link rel="icon" href="../sources/images/white_logo_transparent_background.png" type="image/png">
-    <body>
-    <div class="container">
-        <div class="letras"><h1 style="margin-top: 1%; text-align:center;">Sensores Registrados</h1></div><br>
-        <div class="sesnores">
-            <!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Widget</title>
-</head>
-<body>
-    <table id="sensores">
-        <thead>
-            <th>Sensor</th>
-            <th>Valor</th>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Sensor 1</td>
-                <td><input type="text" name="sensor1" id="sensor1" value="0"></td>
-            </tr>
-            <tr>
-                <td>Sensor 2</td>
-                <td><input type="text" name="sensor1" id="sensor1" value="0"></td>
-            </tr>
-        </tbody>
+    <style>
+      h1 {color:#BBE1FA;
+            text-Align:center;}
+      table, th, td {
+        border: 1px solid white;
+        border-collapse: collapse;
+      }
+      th, td {
+        padding: 5px;
+        text-align: left;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Control de Acceso RFID</h1>
+    <table>
+      <thead>
+  <tr>
+    <th>ID de Tarjeta</th>
+    <th>Último Acceso</th>
+    <th>Estado</th> 
+  </tr>
+</thead>
+<tbody id="card-table-body">
+  <tr>
+    <td colspan="3" align="center">Cargando...</td>
+  </tr>
+</tbody>
     </table>
+    <script>
+      var cardDataRef = new EventSource("https://quickroom-8f067-default-rtdb.firebaseio.com/rfid-access-control/cards.json");
 
+cardDataRef.addEventListener('put', function (e) {
+  var json = JSON.parse(e.data);
+  console.log(json);
 
-<script>
-        var sensores = new EventSource("https://iot-ti51-default-rtdb.firebaseio.com/test.json");
-    sensores.addEventListener('put', function (e) {
-        var json = JSON.parse(e.data);
-        console.log(json);
-        if (json.path == "/"){
-            tbody = document.getElementById("tbody_sensores");
-            for (var key in json.data) {
-                console.log(key);
-                console.log(json.data[key].temperatura);
-                document.getElementById(key).value = json.data[key].humedad ;
-            }
-        }else{
-            s = json.path.split("/");
-            console.log(s[1]);
-            console.log(json.data);
-            document.getElementById(s[1]).value = json.data;
-        }
-    });
-</script>
+  if (json.path == "/") {
+    // Clear table body
+    document.getElementById("card-table-body").innerHTML = "";
 
-</body>
+    for (var key in json.data) {
+      console.log(key);
+      console.log(json.data[key].time);
+      console.log(json.data[key].status);
 
+      var cardRow = document.createElement("tr");
+      var cardIdCell = document.createElement("td");
+      cardIdCell.innerHTML = key;
+      var cardTimeCell = document.createElement("td");
+      cardTimeCell.setAttribute("id", key + "-last-access");
+      var cardTimeString = (json.data[key].time).toLocaleString();
+      cardTimeCell.innerHTML = cardTimeString;
 
+      var cardStatusCell = document.createElement("td");
+      cardStatusCell.setAttribute("id", key + "-status");
+      cardStatusCell.innerHTML = json.data[key].status;
 
+      cardRow.appendChild(cardIdCell);
+      cardRow.appendChild(cardTimeCell);
+      cardRow.appendChild(cardStatusCell);
+      document.getElementById("card-table-body").appendChild(cardRow);
+    }
+  } else {
+    var s = json.path.split("/");
+    console.log(s[1]);
+    console.log(json.data.time);
+    console.log(json.data.status);
+
+    var cardTimeCell = document.getElementById(s[1] + "-last-access");
+    var cardTimeString = new Date(json.data.time).toLocaleString();
+    cardTimeCell.innerHTML = cardTimeString;
+
+    var cardStatusCell = document.getElementById(s[1] + "-status");
+    cardStatusCell.innerHTML = json.data.status;
+  }
+});
+
+    </script>
+  </body>
 </html>
-
-            <footer><div class="ayuda"><a href="ayudaadmin.html" style="color: #BBE1FA;">Ayuda
-                <div class="antes"><a href="../Admin/admin.html" style="color:#BBE1FA; float: right;" >Regresar</a></footer>
-        </div>
-        
-    </div>
-</body>
